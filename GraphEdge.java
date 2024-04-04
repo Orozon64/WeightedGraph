c58a7fc1b3db333ae99b48a0815229b43f95ef23
Orozon64 <128033558+Orozon64@users.noreply.github.com>
2024-04-04 09:35:38 +0200

    Update GraphEdge.java

--- GraphEdge.java
+++ GraphEdge.java
@@ -1,5 +1,8 @@
 public class GraphEdge {
     int weight;
     GraphNode nodeToConnect;
-
+    public GraphEdge(int edge_w, GraphNode node_con){
+        weight = edge_w;
+        nodeToConnect = node_con;
+    }
 }
